843ef407e1a6ae4c2a4ec009cfe3b43243b6b4bb
coco <coco@lacueva.tv>
2018-09-11 16:00:44 -0300

    creditos hit

--- web/templates/hit.php
+++ web/templates/hit.php
@@ -1,5 +1,6 @@
 <article class="hit-legales">
-    <a href="http://hit.com.ar/" target="_blank">
-        hit
+    <a class="brand-hit" href="http://hit.com.ar/" target="_blank">
+        <img class="image-logo" src="<?php echo MAINSURL; ?>/assets/images/hit1.png">
+        <img class="image-logo image-logo2" src="<?php echo MAINSURL; ?>/assets/images/hit2.png">
     </a>
 </article>
